fe0dcc072364aadcabd2a9601426452ecbbd8d99
pramod pogula <pramod.pogula1@gmail.com>
2019-08-10 19:00:11 -0600

    Hello world

---
+++ src/com/pramod/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main (String[] args) {
+
+        System.out.println("Hello Pramod!");
+
+        int myFirstNumber = (50 + 5)+ (4 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+    }
+}
